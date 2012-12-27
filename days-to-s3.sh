a68c2be84608bba754a00f2738a2122b7a81950b
Mark Roddy <markroddy@gmail.com>
2012-12-27 19:46:23 +0000

    script to push a bunch of days in a row to s3

---
+++ days-to-s3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ 3 != $# ]; then
+    echo "usage: $0 yyyy-mm-dd numdays-to-export delay-between-days";
+    exit 1;
+fi
+
+STARTDATE=$1
+NUMDAYS=$2
+DELAY=$3
+
+
+START=`echo $STARTDATE | tr -d -`;
+
+for (( c=0; c<$2; c++ )); do
+    CURDATE=`date --date="$START +$c day" +%Y-%m-%d`;
+    echo "Would Process: $CURDATE ";
+    ./date-to-s3.sh s3://net.ednit.datasets/githubarchive $CURDATE;
+done
