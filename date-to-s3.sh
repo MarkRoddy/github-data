ee2dab631338ae56b709d9353451001de06235c7
Mark Roddy <markroddy@gmail.com>
2012-12-27 19:26:53 +0000

    fix arg checking, fix data extraction and fix wget command

--- date-to-s3.sh
+++ date-to-s3.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-if [ 3 != $# ]; then
+if [ 2 != $# ]; then
     echo "usage: $0 s3://bucket/dir/root yyyy-mm-dd";
     exit 1;
 fi
 
 ROOTBUCKET=$1;
 EVENTDATE=$2;
-YEAR=`cut -d '-' -f 1 $EVENTDATE`
-MONTH=`cut -d '-' -f 2 $EVENTDATE`
-DAY=`cut -d '-' -f 3 $EVENTDATE`
+YEAR=`echo "$EVENTDATE" | cut -d '-' -f 1`
+MONTH=`echo "$EVENTDATE" | cut -d '-' -f 2`
+DAY=`echo "$EVENTDATE" | cut -d '-' -f 3`
 
-wget http://data.githubarchive.org/2012-04-11-{0..23}.json.gz
+wget http://data.githubarchive.org/$DATE-{0..23}.json.gz
 
 S3DEST="$ROOTBUCKET/$YEAR/$MONTH/$DAY/"
 s3cmd put *.json.gz "$S3DEST"
