50399e24bc83b1613b5511e24b6b265033af4dd8
Mark Roddy <markroddy@gmail.com>
2012-12-27 19:29:34 +0000

    download in a temp directory then clean up

--- date-to-s3.sh
+++ date-to-s3.sh
@@ -13,7 +13,13 @@ YEAR=`echo "$EVENTDATE" | cut -d '-' -f 1`
 MONTH=`echo "$EVENTDATE" | cut -d '-' -f 2`
 DAY=`echo "$EVENTDATE" | cut -d '-' -f 3`
 
+TMPDIR=`mktemp --directory`
+pushd $TMPDIR
+
 wget http://data.githubarchive.org/$DATE-{0..23}.json.gz
 
 S3DEST="$ROOTBUCKET/$YEAR/$MONTH/$DAY/"
 s3cmd put *.json.gz "$S3DEST"
+
+popd
+rm -rf $TMPDIR
