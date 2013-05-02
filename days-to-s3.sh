d519dbee06fa30682be7cb86f616ddd99b432d3d
Mark Roddy <markroddy@gmail.com>
2013-05-02 12:29:07 -0400

    date command doesn't work this way on osx, dropped into python

--- days-to-s3.sh
+++ days-to-s3.sh
@@ -11,12 +11,21 @@ STARTDATE=$1
 NUMDAYS=$2
 DELAY=$3
 
-
 START=`echo $STARTDATE | tr -d -`;
 
 for (( c=0; c<$2; c++ )); do
-    CURDATE=`date --date="$START +$c day" +%Y-%m-%d`;
+    # CURDATE=`date --date="$START +$c day" +%Y-%m-%d`;
+    PYFORMATDATE=$(cat <<EOF
+from datetime import datetime, timedelta;
+START_DATE="$STARTDATE"
+y,m,d = map(int, START_DATE.split('-'))
+dt=datetime(y,m,d)
+new_dt=dt + timedelta(days=$c)
+print "%s-%02d-%02d" % (new_dt.year, new_dt.month, new_dt.day)
+EOF
+)
+    CURDATE=`python -c "$PYFORMATDATE"`
     echo "Would Process: $CURDATE ";
-    ./date-to-s3.sh s3://net.ednit.datasets/githubarchive $CURDATE;
+    ./date-to-s3.sh s3://mortar-mroddy-datasets/githubarchive $CURDATE;
     sleep $DELAY;
 done
